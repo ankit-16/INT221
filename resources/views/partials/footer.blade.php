9b8b58385725ccc2694d867dabac3014bfb75bda
Ankit-16 <ankitgupta38017@gmail.com>
2022-04-06 13:21:27 +0530

    Intialized git and added all file to staged and commited

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="bg-dark">
+    <div class="container bg-dark">
+        <div class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="footer-link">Privacy policy</a><br>
+                    <a href="#" class="footer-link">Term and conditions</a><br>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Welcome to the site
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, assumenda. Culpa, libero.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Contact:
+                    </h3>
+                    <p>Phone: +91123456789</p>
+                    <p>Email: admin@ecom.com</p>
+                    <p>Github: <a href="https://github.com/ankit-16">@ankit-16</a></p>
+                </div>
+            </div>
+        </div>
+        <p>copyright &copy; Ankit Gupta - All rights reserved 2022</p>
+    </div>
+</footer>
